e9a15a59e898e7647fbf0e3e0e6795395e1b0333
Abdullah Akhtar <44116978+aaydee99@users.noreply.github.com>
2023-08-01 02:02:29 +0500

    added documentation

--- frontend/src/organisms/bookList/index.tsx
+++ frontend/src/organisms/bookList/index.tsx
@@ -16,7 +16,6 @@ const BookList: React.FC = () => {
 
   return (
     <div>
-      <h1>Book List</h1>
       <div className={`grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6`}>
         {/* Render BookCard components */}
         {books.slice(0, itemsPerPage).map((book) => (
